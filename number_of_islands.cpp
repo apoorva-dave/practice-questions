89fb03f731ca7a413c5f053e12866e2cce949f2d
apoorva-dave <apoorvadave1995@gmail.com>
2020-02-21 20:36:14 +0530

    Number of islands using dfs

---
+++ number_of_islands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dfs(vector<vector<char>> &grid, int i, int j)
+    {
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[i].size() || grid[i][j]=='0')
+            return 0;
+        grid[i][j] = '0';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+        return 1;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.size() < 0)
+            return 0;
+        int num_islands = 0;
+        for(int i=0; i<grid.size();i++)
+        {
+            for(int j=0;j<grid[i].size();j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    num_islands += dfs(grid,i,j);
+                }
+            }
+        }
+        return num_islands;
+    }
+};
